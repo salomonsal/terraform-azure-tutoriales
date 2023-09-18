5f1d71d8372ef1a2f24387f22b30c4f78d630cc8
Salomon Saldivia Rengel <salomonsal@hotmail.com>
2023-09-18 11:56:09 +0100

    Actualización de repositorio
    Se suben todos los archivos ft

---
+++ terraform-azure-resourcegroup/main.tf
@@ -0,0 +1,31 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "AprendeMoncho"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
+  }
+}
---
+++ terraform-azure/main.tf
@@ -0,0 +1,70 @@
+# Creando infraestructura un poco más compleja en Azure
+resource "azurerm_resource_group" "main" {
+  name     = "rcdevopsterraform-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "rc-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "rc-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "rcdevopsvm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  delete_os_disk_on_termination = true
+
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "microsoftwindowsserver"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "rcwinserver"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "rcwinservervm"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_windows_config {
+    enable_automatic_upgrades = true
+    provision_vm_agent        = true
+  }
+
+#  tags  {
+#    environment = "dev"
+#  }
+}
